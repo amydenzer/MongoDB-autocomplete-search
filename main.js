31a045b59b90cf576de2fb2880b0fa9fabccd3e0
amydenzer <amydenzer@gmail.com>
2024-03-09 18:16:00 -0600

    debug class constructor error

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 $(document).ready(function () {
     $('#title').autocomplete({
-        source: async function(request, response) {
-            let data = await fetch(`http://localhost:8000/search?query=${request.term}`)
-            .then(results => results.json())
-            .then(results => results.map(result => {
-                return {
-                    label: result.title,
-                    value: result.title,
-                    id: result._id
-                }
-            }))
-            response(data)
+        source: async function(request,response) {
+            let data= await fetch(`http://localhost:8000/search?query=${request.term}`)
+                    .then(results => results.json())
+                    .then(results => results.map(result => {
+                        return {
+                            label: result.title,
+                            value: result.title,
+                            id: result._id
+                        }
+                    }))
+                response(data)
+                //console.log(response)
         },
         minLength: 2,
         select: function(event, ui) {
@@ -19,13 +20,12 @@ $(document).ready(function () {
                 .then(result => result.json())
                 .then(result => {
                     $('#cast').empty()
-                    result.cast.forEach(cast => {
-                        $(cast).append(`<li>${cast}</li>`)
-                    })
-                    $('img').attr('src', result.poster)
+                    result.cast.forEach(cast =>
+                        {
+                            $("#cast").append(`<li>${cast}</li>`)
+                        })
+                        $('img').attr('src',result.poster)
                 })
         }
     })
-}
-
-)
+})
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {MongoClient, ObjectId} = require('mongodb')
+const {MongoClient, ObjectId } = require('mongodb')
+const { response } = require('express')
+const { request } = require('http')
 require('dotenv').config()
 const PORT = 8000
 
@@ -10,54 +12,54 @@ let db,
     dbName = 'sample_mflix',
     collection
 
-MongoClient.connect(dbConnectionStr)  
+MongoClient.connect(dbConnectionStr)
     .then(client => {
-        console.log('Connected to database')
+        console.log(`Connected to database`)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
-
 
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 app.use(cors())
 
-
-app.get("/search", async (request, response) => {
+app.get("/search", async (request,response) => {
     try {
         let result = await collection.aggregate([
             {
-                "$Search" : {
-                    "autocomplete": {
+                "$search" : {
+                    "autocomplete" : {
                         "query": `${request.query.query}`,
                         "path": "title",
                         "fuzzy": {
-                            "maxEdits": 2,
+                            "maxEdits":2,
                             "prefixLength": 3
                         }
                     }
                 }
             }
         ]).toArray()
+        //console.log(result)
         response.send(result)
     } catch (error) {
         response.status(500).send({message: error.message})
+        //console.log(error)
     }
 })
 
 app.get("/get/:id", async (request, response) => {
     try {
         let result = await collection.findOne({
-            "_id": ObjectId(request.params.id)
+            "_id" : new ObjectId(request.params.id)
         })
         response.send(result)
-    }catch (error) {
+    } catch (error) {
         response.status(500).send({message: error.message})
     }
-})
+}
+)
 
-  
 app.listen(process.env.PORT || PORT, () => {
-    console.log('Server is running.')
-})    
+    console.log(`Server is running.`)
+})
 
